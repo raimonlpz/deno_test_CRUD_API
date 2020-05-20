4df99ea7e9b2b992de4df6f0b02691f33b7f05be
Raimon López <raimonlpez@gmail.com>
2020-05-20 13:05:42 +0200

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,16 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts'
+import router from './routes.ts'
+
+const port = 5000
+
+const app = new Application()
+
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+
+console.log(`Server running on port ${port}`)
+await app.listen({port})
+
+//deno run --allow-net server.ts
